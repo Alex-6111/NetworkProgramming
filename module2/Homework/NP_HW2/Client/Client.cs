79adccc5e5cf2571bfa66a7d7b9091b5b5372535
Oleksandr Bekas <lxbekas@gmail.com>
2023-01-31 19:53:15 +0200

    Complete

---
+++ module2/Homework/NP_HW2/Client/Client.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient client = new TcpClient("localhost", 8080);
+                Console.WriteLine("Connected to server.");
+
+                NetworkStream stream = client.GetStream();
+
+                Console.WriteLine("Enter two currency codes (e.g. USD EUR): ");
+                string message = Console.ReadLine();
+
+                byte[] request = System.Text.Encoding.ASCII.GetBytes(message);
+                stream.Write(request, 0, request.Length);
+
+                byte[] response = new byte[256];
+                int bytesRead = stream.Read(response, 0, response.Length);
+                string rate = System.Text.Encoding.ASCII.GetString(response, 0, bytesRead);
+
+                Console.WriteLine("Exchange rate: " + rate);
+                stream.Close();
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
+
