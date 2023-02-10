4770269c5646c6fe2269344aaa04ca9363c90558
Oleksandr Bekas <lxbekas@gmail.com>
2023-02-10 15:16:34 +0200

    Complete

---
+++ module4/NP_HW6/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace NP_HW6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtChat = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtChat
+            // 
+            this.txtChat.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtChat.Location = new System.Drawing.Point(12, 12);
+            this.txtChat.Multiline = true;
+            this.txtChat.Name = "txtChat";
+            this.txtChat.ReadOnly = true;
+            this.txtChat.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtChat.Size = new System.Drawing.Size(776, 376);
+            this.txtChat.TabIndex = 0;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMessage.Location = new System.Drawing.Point(12, 394);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(696, 22);
+            this.txtMessage.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSend.Location = new System.Drawing.Point(714, 393);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSend;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtChat);
+            this.Name = "ChatForm";
+            this.Text = "Chat Application (UDP)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtChat;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ module4/NP_HW6/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace NP_HW6
+{
+    public partial class Form1 : Form
+    {
+        private readonly int _port = 8000;
+        private readonly UdpClient _client = new UdpClient();
+        private IPEndPoint _remoteIpEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string message = txtMessage.Text;
+            byte[] sendBytes = System.Text.Encoding.ASCII.GetBytes(message);
+
+            _client.Send(sendBytes, sendBytes.Length, _remoteIpEndPoint);
+            txtMessage.Text = "";
+            txtChat.Text += "You: " + message + Environment.NewLine;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _client.Connect(_remoteIpEndPoint);
+            _client.BeginReceive(DataReceived, null);
+        }
+
+        private void DataReceived(IAsyncResult result)
+        {
+            IPEndPoint receiveIpEndPoint = new IPEndPoint(IPAddress.Any, _port);
+            byte[] receivedBytes = _client.EndReceive(result, ref receiveIpEndPoint);
+            string message = System.Text.Encoding.ASCII.GetString(receivedBytes);
+
+            if (InvokeRequired)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    txtChat.Text += receiveIpEndPoint.Address.ToString() + ": " + message + Environment.NewLine;
+                });
+            }
+            else
+            {
+                txtChat.Text += receiveIpEndPoint.Address.ToString() + ": " + message + Environment.NewLine;
+            }
+
+            _client.BeginReceive(DataReceived, null);
+        }
+    }
+}
+
